5cc18640fe53befaebd538d3d6af9da785f1a2bd
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-04 11:58:04 -0600

    C Sharp El area altura volumen de un cilindro

---
+++ El area altura volumen de un cilindro/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace El_area_altura_volumen_de_un_cilindro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label_Area = new System.Windows.Forms.Label();
+            this.Label_Volumen = new System.Windows.Forms.Label();
+            this.TextBox_Radio = new System.Windows.Forms.TextBox();
+            this.TextBox_Altura = new System.Windows.Forms.TextBox();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button_Calcular = new System.Windows.Forms.Button();
+            this.Button_Limpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.Location = new System.Drawing.Point(12, 386);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(76, 20);
+            this.Label4.TabIndex = 47;
+            this.Label4.Text = "Volumen:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(41, 345);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(47, 20);
+            this.Label3.TabIndex = 46;
+            this.Label3.Text = "Area:";
+            // 
+            // Label_Area
+            // 
+            this.Label_Area.AutoSize = true;
+            this.Label_Area.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Area.Location = new System.Drawing.Point(94, 345);
+            this.Label_Area.Name = "Label_Area";
+            this.Label_Area.Size = new System.Drawing.Size(18, 20);
+            this.Label_Area.TabIndex = 45;
+            this.Label_Area.Text = "0";
+            // 
+            // Label_Volumen
+            // 
+            this.Label_Volumen.AutoSize = true;
+            this.Label_Volumen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Volumen.Location = new System.Drawing.Point(94, 386);
+            this.Label_Volumen.Name = "Label_Volumen";
+            this.Label_Volumen.Size = new System.Drawing.Size(18, 20);
+            this.Label_Volumen.TabIndex = 44;
+            this.Label_Volumen.Text = "0";
+            // 
+            // TextBox_Radio
+            // 
+            this.TextBox_Radio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Radio.Location = new System.Drawing.Point(207, 311);
+            this.TextBox_Radio.Name = "TextBox_Radio";
+            this.TextBox_Radio.Size = new System.Drawing.Size(74, 22);
+            this.TextBox_Radio.TabIndex = 43;
+            // 
+            // TextBox_Altura
+            // 
+            this.TextBox_Altura.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Altura.Location = new System.Drawing.Point(341, 183);
+            this.TextBox_Altura.Name = "TextBox_Altura";
+            this.TextBox_Altura.Size = new System.Drawing.Size(74, 22);
+            this.TextBox_Altura.TabIndex = 42;
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox2.Image")));
+            this.PictureBox2.Location = new System.Drawing.Point(358, 14);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(147, 143);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox2.TabIndex = 41;
+            this.PictureBox2.TabStop = false;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox1.Image")));
+            this.PictureBox1.Location = new System.Drawing.Point(12, 14);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(336, 273);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 40;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(221, 288);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(51, 20);
+            this.Label2.TabIndex = 39;
+            this.Label2.Text = "Radio";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(354, 160);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(51, 20);
+            this.Label1.TabIndex = 38;
+            this.Label1.Text = "Altura";
+            // 
+            // Button_Calcular
+            // 
+            this.Button_Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Calcular.Location = new System.Drawing.Point(113, 428);
+            this.Button_Calcular.Name = "Button_Calcular";
+            this.Button_Calcular.Size = new System.Drawing.Size(75, 25);
+            this.Button_Calcular.TabIndex = 37;
+            this.Button_Calcular.Text = "Calcular";
+            this.Button_Calcular.UseVisualStyleBackColor = true;
+            this.Button_Calcular.Click += new System.EventHandler(this.Button_Calcular_Click);
+            // 
+            // Button_Limpiar
+            // 
+            this.Button_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Limpiar.Location = new System.Drawing.Point(299, 428);
+            this.Button_Limpiar.Name = "Button_Limpiar";
+            this.Button_Limpiar.Size = new System.Drawing.Size(75, 25);
+            this.Button_Limpiar.TabIndex = 36;
+            this.Button_Limpiar.Text = "Limpiar";
+            this.Button_Limpiar.UseVisualStyleBackColor = true;
+            this.Button_Limpiar.Click += new System.EventHandler(this.Button_Limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(517, 466);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label_Area);
+            this.Controls.Add(this.Label_Volumen);
+            this.Controls.Add(this.TextBox_Radio);
+            this.Controls.Add(this.TextBox_Altura);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button_Calcular);
+            this.Controls.Add(this.Button_Limpiar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "El area volumen de un cilindro";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label_Area;
+        internal System.Windows.Forms.Label Label_Volumen;
+        internal System.Windows.Forms.TextBox TextBox_Radio;
+        internal System.Windows.Forms.TextBox TextBox_Altura;
+        internal System.Windows.Forms.PictureBox PictureBox2;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button Button_Calcular;
+        internal System.Windows.Forms.Button Button_Limpiar;
+    }
+}
+
---
+++ El area altura volumen de un cilindro/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace El_area_altura_volumen_de_un_cilindro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Cilindro()
+        {
+            //Declararemos 5 variables de tipo double
+            double altura, area, radio, radio2, volumen;
+            //Condición que valida los TextBox si  estos están vacío 
+            if (TextBox_Altura.Text == "")
+            {
+                MessageBox.Show("Ingrese una altura", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //Coloca el Focus en el TextBox_Altura
+                TextBox_Altura.Focus();
+            }
+            else
+            {
+                //Condición que valida los TextBox si  estos están vacío 
+                if (TextBox_Radio.Text == "")
+                {
+                    MessageBox.Show("Ingrese un radio", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //Coloca el Focus en el TextBox_Radio
+                    TextBox_Radio.Focus();
+                }
+                else
+                {
+                    /* Funciones que convierte en un tipo double el valor que se ingrese por 
+                        los TextBox y lo almacena en las variables radio,altura  */
+                    altura = Convert.ToDouble(TextBox_Altura.Text);
+                    radio= Convert.ToDouble(TextBox_Radio.Text);
+                    //Metodo para calcular el área del cilindro
+                    area = (2 * Math.PI * radio * (altura + radio));
+                    /* Eleva al cuadrado el dato almacenado en las variable 
+                          radio y lo almacena  en la variable radio2  */
+                    radio2 = Math.Pow(radio, 2);
+                    //Método para calcular el volumen del cilindro
+                    volumen = (Math.PI * radio2 * altura);
+                    //Mostrar los resultados en la label
+                    Label_Area.Text = area.ToString();
+                    Label_Volumen.Text = volumen.ToString();
+                }
+            }
+        }
+
+        private void Button_Limpiar_Click(object sender, EventArgs e)
+        {
+            //Restablecer los controles 
+            TextBox_Altura.Text = ""	;
+            TextBox_Radio.Text = "";
+            Label_Area.Text = "0";
+            Label_Volumen.Text = "0";
+            TextBox_Altura.Focus();
+        }
+
+        private void Button_Calcular_Click(object sender, EventArgs e)
+        {
+            Cilindro();
+        }
+     }
+}
+
+ 
+ 
